ce703ee80b44c1ca1787a76c4206af4acab79aff
Cristopher <cristopher.correo@gmail.com>
2020-07-06 21:07:38 +0100

    App component using redux

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
 import './App.css';
+import { connect } from 'react-redux';
+import { addProduct } from './actions';
 
-
-const App = () => {
-  const products = {
+const App = ({ products, addProduct }) => {
+  const productsList = {
     Beans: '0.50',
     Coke: '0.70',
     Orange: '1.99/kg'
   };
-  // const products = ['Beans', 'Coke', 'Orange'];
+
   return (
     <div className="App">
       <ul>
-        {Object.keys(products).map(title => (
+        {Object.keys(productsList).map(title => (
           <li className="product-item" key={title}>
             <span className="product-title">{title}</span>
-            <span className="product-price">{products[title]}</span>
-            <button className="add-btn">add</button>
+            <span className="product-price">{productsList[title]}</span>
+            <button className="add-btn" onClick={() => addProduct(title)}>add</button>
+            <span className=".product-counter"> {products[title] ? products[title] : 0} </span>
           </li>
         ))}
       </ul>
@@ -24,4 +26,8 @@ const App = () => {
   );
 }
 
-export default App;
+const mapStateToProps = state => ({
+  products: state.products
+})
+
+export default connect(mapStateToProps, { addProduct })(App);
