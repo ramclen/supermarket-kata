869c8b41a3a80896e56fdb68dd6e358f9b955d49
Cristopher <cristopher.correo@gmail.com>
2020-07-06 20:08:10 +0100

    Passed price test

--- src/App.js
+++ src/App.js
@@ -3,13 +3,19 @@ import './App.css';
 
 
 const App = () => {
-  const products = ['Beans', 'Coke', 'Orange'];
+  const products = {
+    Beans: '0.50',
+    Coke: '0.70',
+    Orange: '1.99/kg'
+  };
+  // const products = ['Beans', 'Coke', 'Orange'];
   return (
     <div className="App">
       <ul>
-        {products.map(product => (
-          <li className="product-item" key={product}>
-            <span className="product-title">{product}</span>
+        {Object.keys(products).map(title => (
+          <li className="product-item" key={title}>
+            <span className="product-title">{title}</span>
+            <span className="product-price">{products[title]}</span>
             <button className="add-btn">add</button>
           </li>
         ))}
