fc0e433f810d2beb4b8d0cc695af964ff145b659
Cristopher <cristopher.correo@gmail.com>
2020-07-06 18:45:04 +0100

    Starting test with enzyme

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,11 @@
 import React from 'react';
-import { render } from '@testing-library/react';
 import App from './App';
+import { shallow } from 'enzyme';
 
-test('renders learn react link', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe("App", () => {
+  it("should have hello world text", () => {
+    const appWrapper = shallow(<App />);
+    expect(appWrapper.find('.App').text()).toBe('Hello world')
+  })
+})
+
