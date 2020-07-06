62e6e8ce3002e1eeda71b7cc8cf11831dd8a05d6
Cristopher <cristopher.correo@gmail.com>
2020-07-06 19:21:44 +0100

    create first set of tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import App from './App';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 
 describe("App", () => {
-  it("should have hello world text", () => {
+  it("should have a list of products", () => {
     const appWrapper = shallow(<App />);
-    expect(appWrapper.find('.App').text()).toBe('Hello world')
+    expect(appWrapper.exists('.product-item')).toBe(true)
+  })
+
+  it("should show the products that supermarket has", () => {
+    const appWrapper = shallow(<App />);
+    expect(appWrapper.find('.product-item')).toHaveLength(4);
   })
 })
 
