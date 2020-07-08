ad9e3f127a3ec6ae3989447d4a1be5683f054e49
Cristopher <cristopher.correo@gmail.com>
2020-07-08 18:42:59 +0100

    Moving tests to correct component

--- src/App.test.js
+++ src/App.test.js
@@ -5,7 +5,7 @@ import { shallow, mount } from 'enzyme';
 import { addProduct } from './actions';
 import { Provider } from 'react-redux';
 
-describe("Products", () => {
+describe("App", () => {
   let appWrapper;
   let store;
   let state;
@@ -26,37 +26,4 @@ describe("Products", () => {
     expect(['Coke', 'Orange', 'Beans'].sort()).toEqual(products.sort());
   })
 
-  it("should have a button with 'add' text", () => {
-    expect(appWrapper.exists('.add-btn')).toBe(true)
-    appWrapper.find('.add-btn').forEach(item => expect(item.text()).toBe('add'));
-
-  })
-
-  it("should have the correct price per product", () => {
-    const prices = {
-      beans: '0.50',
-      coke: '0.70',
-      orange: '1.99/kg'
-    };
-
-    expect(appWrapper.find(".product-price")).toHaveLength(3);
-    appWrapper.find(".product-item").forEach(item => {
-      const productTitle = item.find('.product-title').text().toLowerCase();
-      const expectedPrice = prices[productTitle];
-      expect(item.find('.product-price').text()).toBe(expectedPrice)
-    })
-  })
-
-  it("should show how many has been selected", () => {
-    const firstItem = appWrapper.find(".product-item").first();
-    const productTitle = firstItem.find('.product-title').text();
-    state = { products: { [productTitle]: 1 } }
-    const addProductAction = addProduct(productTitle);
-
-    firstItem.find('.add-btn').simulate('click');
-    expect(store.dispatch).toHaveBeenCalledTimes(1);
-    expect(store.dispatch).toHaveBeenCalledWith(addProductAction);
-
-    expect(parseInt(firstItem.find('.product-counter').text())).toBe(1);
-  })
 })
---
+++ src/components/ProductList.test.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import configureStore from "redux-mock-store";
+import { mount } from 'enzyme';
+import { addProduct } from '../actions';
+import { Provider } from 'react-redux';
+import ProductList from './ProductList';
+
+describe("Products", () => {
+  let productListWrapper;
+  let store;
+  let state;
+  const productsList = {
+    beans: '0.50',
+    coke: '0.70',
+    orange: '1.99/kg'
+  };
+  beforeEach(() => {
+    state = { products: {} }
+    store = configureStore()(() => state);
+    store.dispatch = jest.fn(store.dispatch);
+    productListWrapper = mount(<Provider store={store}><ProductList productsList={productsList} /></Provider>);
+  })
+
+  it("should have a button with 'add' text", () => {
+    expect(productListWrapper.exists('.add-btn')).toBe(true)
+    productListWrapper.find('.add-btn').forEach(item => expect(item.text()).toBe('add'));
+  })
+
+  it("should have the correct price per product", () => {
+    expect(productListWrapper.find(".product-price")).toHaveLength(Object.keys(productsList).length);
+    productListWrapper.find(".product-item").forEach(item => {
+      const productTitle = item.find('.product-title').text().toLowerCase();
+      const expectedPrice = productsList[productTitle];
+      expect(item.find('.product-price').text()).toBe(expectedPrice)
+    })
+  })
+
+  it("should show how many has been selected", () => {
+    const firstItem = productListWrapper.find(".product-item").first();
+    const productTitle = firstItem.find('.product-title').text();
+    state = { products: { [productTitle]: 1 } }
+    const addProductAction = addProduct(productTitle);
+
+    firstItem.find('.add-btn').simulate('click');
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+    expect(store.dispatch).toHaveBeenCalledWith(addProductAction);
+
+    expect(parseInt(firstItem.find('.product-counter').text())).toBe(1);
+  })
+})
