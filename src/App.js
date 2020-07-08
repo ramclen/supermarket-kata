5996042f5e44d708a5d8e6d8ae87b3e1c5912541
Cristopher <cristopher.correo@gmail.com>
2020-07-08 20:38:12 +0100

    calculating the subtotal

--- src/App.js
+++ src/App.js
@@ -2,23 +2,32 @@ import React from 'react';
 import './App.css';
 import { connect } from 'react-redux';
 import ProductsList from './components/ProductList';
+import { render } from 'enzyme';
 
 export const App = ({ products }) => {
-  const productsList = {
+  const productPrices = {
     Beans: '0.50',
     Coke: '0.70',
     Orange: '1.99/kg'
   };
 
-  products
+  const calculateSubTotal = () => {
+    const subtotal = Object
+      .entries(productPrices)
+      .map(([name, price]) => (products[name] || 0) * parseFloat(price))
+      .reduce((acc, price) => {
+        return acc + price
+      }, 0)
+    return (Math.round(subtotal * 100) / 100).toFixed(2);
+  }
 
   return (
     <div className="App">
-      <ProductsList productsList={productsList} />
+      <ProductsList productsList={productPrices} />
       <div className="price-section">
         <div className="subtotal-section">
           <h3>Sub - Total</h3>
-          <span id="subtotal">1.70</span>
+          <span id="subtotal">{calculateSubTotal()}</span>
         </div>
 
         <div className="savings-section">
