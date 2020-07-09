b7be65fd8c3b3fbff25cbadb2c39b3c610cc055d
Cristopher <cristopher.correo@gmail.com>
2020-07-09 21:01:26 +0100

    Passed tests and added comments

--- src/services/PriceHandler.js
+++ src/services/PriceHandler.js
@@ -3,10 +3,24 @@ export class PriceHandler {
     this.prices = prices;
   }
 
+  /**
+   * it calculates subTotal from a bill using prices on the constructor
+   * @param  {} bill
+   */
   calculateSubTotal(bill) {
     return Object
       .keys(bill)
       .map(name => bill[name] * parseFloat(this.prices[name]))
-      .reduce((acc, price) => acc + price, 0)
+      .reduce((acc, price) => acc + price, 0);
   }
+
+  /**
+   * It calculates the total, if discount passed will subtract from the total
+   * @param  {} bill
+   * @param  {number} discount = 0
+   */
+  calculateTotal(bill, discount = 0) {
+    return this.calculateSubTotal(bill) - discount;
+  }
+
 }
