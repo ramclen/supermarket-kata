4a4bd6c9ca08053eaeeb25ae9fa8d9fd9627aa09
Cristopher <cristopher.correo@gmail.com>
2020-07-09 19:47:01 +0100

    Passing the test and adding method comments

--- src/services/DiscountHandler.js
+++ src/services/DiscountHandler.js
@@ -5,15 +5,27 @@ export class DiscountHandler {
     this.discounts = discounts;
   }
 
+  /**
+   * It returns the total discount that would produce the passed bill
+   * @param  {} bill
+   * @returns {number}
+   */
   getTotalSavings(bill) {
     return Object.keys(this.discounts)
       .map(name => this.discounts[name](bill[name] ? bill[name] : 0))
       .reduce((acc, discount) => acc + parseFloat(discount), 0)
   }
+
+  /**
+   * It returns an object with all applicable discount for the passed bill.
+   * Object is structure with keys as the name of product and the total discount is the value
+   * @param  {Object} bill
+   * @returns {Object} discounts
+   */
+  getApplicableDiscounts(bill) {
+    return Object.keys(this.discounts)
+      .map(name => ({ name, total: this.discounts[name](bill[name]) }))
+      .filter(discount => discount.total > 0)
+      .reduce((obj, discount) => ({ ...obj, [discount.name]: discount.total }), {})
+  }
 }
-
-// const discounts = {
-//   Beans: (amount) => forceTwoDigits(-Math.floor(amount / 3) * 0.5),
-//   Coke: (amount) => forceTwoDigits(-Math.floor(amount / 2) * 0.4)
-// }
-
