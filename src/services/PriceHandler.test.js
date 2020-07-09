50f2c5a9d8a8587623bec3225df6b82958f84db0
Cristopher <cristopher.correo@gmail.com>
2020-07-09 20:53:27 +0100

    Creating test to calculate total with discount

--- src/services/PriceHandler.test.js
+++ src/services/PriceHandler.test.js
@@ -1,19 +1,33 @@
 const { PriceHandler } = require("./PriceHandler");
 
 describe("Price Handler", () => {
-  it("should calculate prices from bill", () => {
-    const prices = {
+  let prices;
+  let priceHandler;
+
+  beforeEach(() => {
+    prices = {
       Beans: '0.50',
       Coke: '0.70',
       Orange: '1.99/kg'
     }
+    priceHandler = new PriceHandler(prices);
+  })
 
+  it("should calculate prices from bill", () => {
     const bill = {
       Beans: 2,
       Coke: 1
     }
-    const priceHandler = new PriceHandler(prices);
 
     expect(priceHandler.calculateSubTotal(bill)).toBe(1.7);
+  })
+
+  it("should calculate total if total discount is passed", () => {
+    const bill = {
+      Beans: 3,
+      Coke: 2
+    }
+
+    expect(priceHandler.calculateTotal(bill, 0.9)).toBe(2);
   })
 })
