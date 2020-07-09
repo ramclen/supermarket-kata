3c6db5cfcae473adabf209cc34d9d773811dac9e
Cristopher <cristopher.correo@gmail.com>
2020-07-09 19:12:15 +0100

    creating test to get applicable discounts

--- src/services/DiscountHandler.test.js
+++ src/services/DiscountHandler.test.js
@@ -1,19 +1,30 @@
-const { DiscountHandler } = require("./DiscountHandler")
+const { DiscountHandler } = require("./DiscountCalculator")
 
 describe("Discount Handler", () => {
   const discounts = {
     Beans: (amount) => Math.floor(amount / 3) * 0.5,
     Coke: (amount) => Math.floor(amount / 2) * 0.4
   }
+  let discountHandler;
+
+  beforeEach(() => {
+    discountHandler = new DiscountHandler(discounts);
+  })
 
   it("should be possible to return total savings", () => {
     const bill = {
       Coke: 5,
       Beans: 6
     }
-    const discountHandler = new DiscountHandler(discounts);
     expect(discountHandler.getTotalSavings(bill)).toBe(1.8);
   })
 
-
+  it("should be possible to get applicable discounts", () => {
+    const bill = {
+      Coke: 2,
+      Beans: 3
+    }
+    expect(discountHandler.getApplicableDiscounts(bill).Coke.total).toBe(0.4);
+    expect(discountHandler.getApplicableDiscounts(bill).Beans.total).toBe(0.5);
+  })
 })
