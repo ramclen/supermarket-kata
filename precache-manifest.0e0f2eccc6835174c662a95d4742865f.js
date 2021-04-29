e5ab6897fc44681dcbd890112c0ea9a5b6ef9dac
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2021-04-29 17:41:28 +0000

    Deploy ramclen/supermarket-kata to github.com/ramclen/supermarket-kata.git:gh-pages

---
+++ precache-manifest.0e0f2eccc6835174c662a95d4742865f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bde7b89966209c596a4d7bf6f897f3d",
+    "url": "/supermarket-kata/index.html"
+  },
+  {
+    "revision": "0b72864626bc847ea05d",
+    "url": "/supermarket-kata/static/css/main.8af3c431.chunk.css"
+  },
+  {
+    "revision": "cbe8d48bc736df0c2477",
+    "url": "/supermarket-kata/static/js/2.41b48916.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/supermarket-kata/static/js/2.41b48916.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0b72864626bc847ea05d",
+    "url": "/supermarket-kata/static/js/main.29c72a7d.chunk.js"
+  },
+  {
+    "revision": "3a74dc939ec0da287a3d",
+    "url": "/supermarket-kata/static/js/runtime-main.063da799.js"
+  }
+]);
